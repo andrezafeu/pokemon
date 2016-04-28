e2d2bc81f67bd08497e307c4cb54f6b92e5e42c7
Andreza Feu <andrezafeu@gmail.com>
2016-04-28 14:11:33 -0400

    Show pokemon info on modal

--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -18,11 +18,22 @@ PokemonApp.Pokemon.prototype.render = function () {
 	$.ajax({
 		url: "/api/pokemon/" + this.id,
 		success: function (response) {
-			self.info = response
+			self.info = response;
 			console.log("Pokemon info:");
 			console.log(self.info);
-
+			$(".js-height").text(self.info.height);
+			$(".js-weight").text(self.info.weight);
+			$(".js-hp").text(self.info.hp);
+			$(".js-sp-atk").text(self.info.sp_atk);
+			$(".js-sp-def").text(self.info.sp_def);
+			$(".js-attack").text(self.info.attack);
+			$(".js-defense").text(self.info.defense);
+			$(".js-speed").text(self.info.speed);
+			// $(".js-types").text(self.info.types);
+			//do append li to show the types of pokemon
+			displayTypes(self.info.types);
 			$(".js-pokemon-modal").modal("show");
+			
 		},
 		error: function (error) {
 			console.log("Error");
@@ -31,6 +42,17 @@ PokemonApp.Pokemon.prototype.render = function () {
 	})
 
 };
+
+function displayTypes (types) {
+	console.log(types)
+	types.forEach(function (oneType) {
+		var types = `
+		    <li>
+		    	${oneType.name}
+		    </li>`;
+		    $(".js-types-list").append(types);
+	});
+}
 
 //This is a function that we will be calling
 PokemonApp.Pokemon.idFromUri = function (pokemonUri) {
@@ -41,6 +63,7 @@ PokemonApp.Pokemon.idFromUri = function (pokemonUri) {
 
 $(document).on("ready", function() {
 	$(".js-show-pokemon").on("click", function (event) {
+		$('.js-types-list').empty();
 		var $button = $(event.currentTarget);
 		var pokemonUri = $button.data("pokemon-uri");
 		// It's making a  new instance of the class we created

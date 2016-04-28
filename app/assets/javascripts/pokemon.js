47209ca37eda7dd946efebdf2f0caa24719c4d11
Andreza Feu <andrezafeu@gmail.com>
2016-04-28 10:46:43 -0400

    Setup class for the pokemon component

--- app/assets/javascripts/pokemon.js
+++ app/assets/javascripts/pokemon.js
@@ -1,2 +1,19 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+//Setup a class for the pokemon component
+
+PokemonApp.Pokemon = function (pokemonUri) {
+	this.id = PokemonApp.Pokemon.idFromUri(pokemonUri);
+};
+
+PokemonApp.Pokemon.prototype.render = function () {
+	console.log("Rendering pokemon: #" + this.id);
+};
+
+PokemonApp.Pokemon.idFromUri = function (pokemonUri) {
+	var uriSegments = pokemonUri.split("/");
+	var secondLast = uriSegments.length - 2;
+	return uriSegments[secondLast];
+};
